6b55b9fa47f63f75d438e4ad4921192f26b38262
jc6w <jc6w@mtmail.mtsu.edu>
2018-07-06 11:16:13 -0500

    Initial check-in of module RevampSearchandScrape

---
+++ RevampSearchandScrape/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace RevampSearchandScrape
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string browser = "";
+            string website = "";
+            string searchTerm = "";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please choose from the following browsers");
+
+            Browse browse = new Browse(browser, website);
+
+            browse.SearchBox(searchTerm);
+
+            Console.WriteLine("blahblaeh");
+        }
+    }
+}
---
+++ RevampSearchandScrape/Properties/Browse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Opera;
+
+namespace RevampSearchandScrape
+{
+    public class Browse
+    {
+        static IWebDriver driver;
+        protected static IWebElement searchBox;
+
+        public Browse(string browser, string website)
+        {
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                //case "Edge": driver = new EdgeDriver();
+                //break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "Opera":
+                    driver = new OperaDriver();
+                    break;
+                case "Safari":
+                    driver = new SafariDriver();
+                    break;
+                default:
+                    Console.WriteLine("INVALID BROWSER");
+                    break;
+            }
+
+            driver.Url = "https://" + website;
+            Setup();
+        }
+
+        void Setup()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(0));
+        }
+
+        public void SearchBox(string searchTerm)
+        {
+            searchBox = driver.FindElement(By.CssSelector("input[type='text']"));
+
+            searchBox.SendKeys(searchTerm);
+        }
+    }
+}
