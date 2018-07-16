4e046e9bd442c00fee0647442f9720dea931df4e
jc6w <jc6w@mtmail.mtsu.edu>
2018-07-16 17:04:27 -0500

    Added LoggingInterceptor

--- RevampSearchandScrape/ContainerUnity.cs
+++ RevampSearchandScrape/ContainerUnity.cs
@@ -1,26 +1,68 @@
-﻿using OfficeOpenXml;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Unity;
+using Unity.Interception.ContainerIntegration;
+using Unity.RegistrationByConvention;
+using Unity.Interception.Interceptors.InstanceInterceptors.InterfaceInterception;
+using Unity.Registration;
+using System;
 
 namespace RevampSearchandScrape
 {
     public class ContainerUnity
     {
-        public static void RegisterElements(IUnityContainer unityContainer)
+        public static IUnityContainer Initialize()
         {
-            ExcelPackage package = new ExcelPackage();
-            unityContainer.RegisterInstance(package);
+            var container = BuildUnityContainer();
 
-            unityContainer.RegisterType<ITest, AmazonTest>("Amazon");
-            unityContainer.RegisterType<IWebDriver, ChromeDriver>("Chrome");
-
-
-
-
-            var testType = typeof(ITest);
-            var browserType = typeof(IWebDriver);
-
+            return container;
         }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+
+            container.AddNewExtension<Interception>();
+
+            //Hack here: Added custom extension method to solve circular dependency issue for injection members. This matching interface checks for name of class that matches the interface name.
+            container.RegisterTypes(
+            AllClasses.FromLoadedAssemblies().WithMatchingInterface(),
+            WithMappings.FromMatchingInterface,
+            WithName.Default,
+            WithLifetime.PerResolve, getInjectionMembers: c => new InjectionMember[]
+                {
+                    new Interceptor<InterfaceInterceptor>(),
+                    new InterceptionBehavior<LoggingInterceptor>()
+                });
+
+            container = RegisterContainer(container);
+
+            //container.AddNewExtension<LazyExtension>();
+
+            return container;
+        }
+
+        private static UnityContainer RegisterContainer(UnityContainer container)
+        {
+            //container.RegisterType(typeof(IConnectionFactory),typeof(ConnectionFactory),new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptor>());
+            //container.RegisterType(typeof(IQueryRepositoryDapper),typeof(QueryRepositoryDapper),new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptor>());
+            //container.RegisterType(typeof(ICommandRepositoryDapper), typeof(CommandRepositoryDapper),new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptor>());
+            //container.RegisterType(typeof(IQueryEmailInformation), typeof(QueryEmailInformation),new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptor>());
+            //container.RegisterType(typeof(ICommandUserRepository), typeof(CommandUserRepository),new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptor>());
+            //container.RegisterType(typeof(IQueryUserRepository), typeof(QueryUserRepository),new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptor>());
+
+            return container;
+        }
+
+    }
+    public static class TypeFilters
+    {
+        public static IEnumerable<Type> WithMatchingInterface(this IEnumerable<Type> types)
+        {
+            return types.Where(type =>
+                type.GetTypeInfo().GetInterface("I" + type.Name) != null);
+        }
+    }
     }
 }
---
+++ RevampSearchandScrape/LoggingInterceptor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+using log4net;
+using System.Diagnostics;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+using Newtonsoft.Json;
+
+namespace RevampSearchandScrape
+{
+    public class LoggingInterceptor: IInterceptionBehavior
+    {
+        private static readonly ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private delegate void logPriorMethodCallDelegate(IMethodInvocation input);
+        private delegate void logAfterMethodCallDelegate(IMethodInvocation input, IMethodReturn methodReturn, Stopwatch stopwatch);
+        public LoggingInterceptor()
+        {
+
+        }
+
+        public Unity.Interception.PolicyInjection.Pipeline.IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            logPriorMethodCallDelegate priorMethodCallObject = new logPriorMethodCallDelegate(LogPriorMethodCall);
+            logAfterMethodCallDelegate afterMethodCallObject = new logAfterMethodCallDelegate(LogAfterMethodCall);
+
+
+            // Code here will execute prior to the method that was called.
+
+            /*Every time a method that is going to be intercepted is invoked, 
+             instead of triggering the method,  Unity calls this Invoke method. 
+             Input parameter (IMethodInvocation) contains all the information about 
+             the method such as it’s name, it’s parameters.*/
+
+            Task PriorMethodCall = Task.Factory.StartNew(() => priorMethodCallObject(input));
+            PriorMethodCall.Wait();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Invoke the next behavior in the chain.
+            //This GetNext is a delegate holding a pointer to the actual method should be called.
+            var methodReturn = getNext().Invoke(input, getNext);
+
+            // Code here will execute after the method that was called.
+            stopwatch.Stop();
+
+            Task AfterMethodCall = Task.Factory.StartNew(() => afterMethodCallObject(input, methodReturn, stopwatch));
+            AfterMethodCall.Wait();
+
+            return methodReturn;
+        }
+
+
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            //This method returns the interfaces required by the behavior for the intercepted objects.
+            //logger.Info(String.Format("[{0}:{1}]", this.GetType().Name, "GetRequiredInterfaces"));
+            return Type.EmptyTypes;
+        }
+
+        public bool WillExecute
+        {
+            //This property is used to optimize proxy creation. It simply returns a flag indicating if the behavior will actually do anything when invoked, and if not, enables the interception mechanism to skip the behavior
+            get
+            {
+                //logger.Info(String.Format("[{0}:{1}]", this.GetType().Name, "WillExecute"));
+                return true;
+            }
+        }
+
+        private void LogPriorMethodCall(IMethodInvocation input)
+        {
+            logger.Info(String.Format("Method Invoked: {0}", input.MethodBase.Name));
+
+            for (int i = 0; i < input.Inputs.Count; i++)
+            {
+                var type = input.Inputs.GetType();
+                logger.Info(String.Format("Inputs Data:  {0} : {1} ", input.Inputs.ParameterName(i), JsonConvert.SerializeObject(input.Inputs[i])));
+            }
+        }
+
+        private void LogAfterMethodCall(IMethodInvocation input, IMethodReturn methodReturn, Stopwatch stopwatch)
+        {
+            if (methodReturn.Exception != null)
+            {
+                logger.Info(methodReturn.ReturnValue?.ToString());
+                Task.Factory.StartNew(() => logger.Error(String.Format("Method {0} threw exception {1} with stack trace {2} at {3}", input.MethodBase, methodReturn.Exception?.Message, methodReturn.Exception?.StackTrace, DateTime.Now.ToLongTimeString())));
+
+                methodReturn.Exception.FlattenHierarchy().ToList().ForEach(innerEx => {
+                    Task.Factory.StartNew(() => logger.Error(String.Format(" Method {0} threw exception {1} at {2}", input.MethodBase, innerEx.Message, DateTime.Now.ToLongTimeString())));
+                });
+
+            }
+            else
+            {
+                logger.Info(String.Format("Method {0} returned {1} at {2}", input.MethodBase, methodReturn?.ReturnValue, DateTime.Now.ToLongTimeString()));
+            }
+        }
+    }
+}
