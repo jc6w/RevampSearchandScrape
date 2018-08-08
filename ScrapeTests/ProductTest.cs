acc82b262c8384a41a229828701a9f216608c51b
jc6w <jc6w@mtmail.mtsu.edu>
2018-08-08 11:40:11 -0500

    Added additional project for testing

---
+++ ScrapeTests/ProductTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+namespace ScrapeTests
+{
+    [TestFixture]
+    public class ProductTest
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
